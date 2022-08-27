940a2596401adb2be43bf13ab4c6fba6065c7791
Tomas Nocetti <tnocetti@fi.uba.ar>
2022-08-27 20:19:00 -0300

    feat: add product description to pdf, invert coord system for correct functioning

--- src/pdf.py
+++ src/pdf.py
@@ -1,14 +1,13 @@
-from cgitb import text
-from reportlab.lib.styles import getSampleStyleSheet
+
+from reportlab.platypus import Table, TableStyle, Frame
+from reportlab.lib import colors
+from reportlab.pdfgen.canvas import Canvas
 from reportlab.lib.pagesizes import A4
 import qrcode
-from reportlab.pdfgen.canvas import Canvas
 
 
 width, height = A4  # keep for later
 
-styleSheet = getSampleStyleSheet()
-style = styleSheet['BodyText']
 
 canv = Canvas('doc.pdf', bottomup=0)
 
@@ -69,7 +68,10 @@ class PdfGenerator():
         self.cae = cae
         self.vto_cae = vto_cae
 
-        self.canv = Canvas('doc.pdf', bottomup=0)
+        self.canv = Canvas('doc.pdf', bottomup=1)
+
+    def inv(self, size):
+        return height - size
 
     def print(self):
         pages = ['ORIGINAL', 'DUPLICADO', 'TRIPLICADO']
@@ -80,6 +82,7 @@ class PdfGenerator():
             self.generate_lef_side_of_header()
             self.generate_date_information()
 
+            self.generate_items_info()
             self.generate_client_information()
             self.generate_total()
             self.generate_footer()
@@ -92,20 +95,21 @@ class PdfGenerator():
 
         self.canv.setLineWidth(SROKE_WITH)
         self.canv.setStrokeColor(STROKE_COLOR)
-        self.canv.rect(LEFT_MARGIN, TOP_MARGIN, width -
+        self.canv.rect(LEFT_MARGIN, self.inv(TOP_MARGIN + HEADER_HEIGHT), width -
                        LEFT_MARGIN * 2, HEADER_HEIGHT, stroke=1)
 
         # Central divider inside header
-        self.canv.line(width/2, 90, width/2, 167)
+        self.canv.line(width/2, self.inv(90), width/2, self.inv(167))
 
         # Horizonal divider inside header
         self.canv.setFont(BOLD_FONT, 16)
-        self.canv.drawCentredString(width/2, 42, page_n)
-        self.canv.line(LEFT_MARGIN, 49, width - LEFT_MARGIN, 49)
+        self.canv.drawCentredString(width/2, self.inv(42), page_n)
+        self.canv.line(LEFT_MARGIN, self.inv(
+            49), width - LEFT_MARGIN, self.inv(49))
 
         # Time Container, Payment Vto. Section
         TC_HEIGHT = 21
-        self.canv.rect(LEFT_MARGIN, 170, width -
+        self.canv.rect(LEFT_MARGIN, self.inv(170 + TC_HEIGHT), width -
                        LEFT_MARGIN * 2, TC_HEIGHT, stroke=1)
 
         # Ticket Type
@@ -113,42 +117,64 @@ class PdfGenerator():
         CONT_WIDTH = 46
         self.canv.setFont(BOLD_FONT, 28)
 
-        self.canv.rect(width/2 - CONT_WIDTH/2, 49, CONT_WIDTH, 41, stroke=1)
-        self.canv.drawString(width/2 - 10, 49 + 26, self.type)
+        self.canv.rect(width/2 - CONT_WIDTH/2, self.inv(49 + 41),
+                       CONT_WIDTH, 41, stroke=1)
+        self.canv.drawString(width/2 - 10, self.inv(49 + 26), self.type)
         self.canv.setFont(BOLD_FONT, 7)
-        self.canv.drawString(width/2 - 14, 49 + 37, 'COD. 011')
+        self.canv.drawString(width/2 - 14, self.inv(49 + 37), 'COD. 011')
 
     # Info
 
+    def generate_items_info(self):
+        elements = []
+        column_width = [43, 283, 65, 80, 94]
+        header = ['Código', 'Producto/Servicio',
+                  'Cantidad', 'Precio Unit.', 'Subtotal']
+        product = ['', 'Servicios Web', '1', '5200', '5200']
+
+        data = [header, product]
+        t = Table(data, colWidths=column_width)
+        t.setStyle(TableStyle([('GRID', (0, 0), (7, 0), 0.5, colors.black),
+                               ('TOPPADDING', (0, 0), (7, 0), 4),
+                               ('BOTTOMPADDING', (0, 0), (7, 0), 4),
+                               ('VALIGN', (0, 0), (7, 0), 'MIDDLE'),
+                               ('BACKGROUND', (0, 0), (7, 0), colors.lightgrey)]))
+        elements.append(t)
+        f = Frame(LEFT_MARGIN, self.inv(260 + 200), width=width -
+                  LEFT_MARGIN * 2, height=200)
+        f.addFromList(elements, self.canv)
+
     def generate_right_side_of_header(self):
         self.canv.setFont(BOLD_FONT, 22)
-        self.canv.drawString(width/2 + 35, 49 + 30, 'FACTURA')
+        self.canv.drawString(width/2 + 35, self.inv(49 + 30), 'FACTURA')
 
         self.canv.setFont(BOLD_FONT, 9)
-        self.canv.drawString(width/2 + 35, 49 + 52,
+        self.canv.drawString(width/2 + 35, self.inv(49 + 52),
                              f'Punto de Venta:  {self.pto_v}   Comp Nro:  {self.ticket_n}')
 
-        self.canv.drawString(width/2 + 35, 49 + 64,
+        self.canv.drawString(width/2 + 35, self.inv(49 + 64),
                              f'Fecha de emisión:  {self.date}')
 
         self.canv.setFont(FONT, 9)
-        self.canv.drawString(width/2 + 35, 49 + 88, f'Doc.:  {self.cuit}')
-        self.canv.drawString(width/2 + 35, 49 + 100,
+        self.canv.drawString(
+            width/2 + 35, self.inv(49 + 88), f'Doc.:  {self.cuit}')
+        self.canv.drawString(width/2 + 35, self.inv(49 + 100),
                              f'Ingresos Brutos:  {self.ib}')
-        self.canv.drawString(width/2 + 35, 49 + 112,
+        self.canv.drawString(width/2 + 35, self.inv(49 + 112),
                              f'Fecha de Inicio de Actividades:  {self.ia}')
 
     def generate_lef_side_of_header(self):
+        BASE = 49
         self.canv.setFont(BOLD_FONT, 22)
-        self.canv.drawString(LEFT_MARGIN * 2, 49 + 30, self.name)
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(BASE + 30), self.name)
 
         self.canv.setFont(FONT, 9)
-        self.canv.drawString(LEFT_MARGIN * 2, 49 + 88,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(BASE + 88),
                              f'Domicilio Comercial:  {self.address}')
-        self.canv.drawString(LEFT_MARGIN * 2, 49 + 64,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(BASE + 64),
                              f'Razón Social:  {self.name}')
         self.canv.setFont(BOLD_FONT, 9)
-        self.canv.drawString(LEFT_MARGIN * 2, 49 + 112,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(BASE + 112),
                              'Condición frente al IVA:  Responsable Monotributo')
 
     def generate_date_information(self):
@@ -157,46 +183,47 @@ class PdfGenerator():
         text_len = canv.stringWidth(
             'Período Facturado Desde:  ', BOLD_FONT, 10)
 
-        self.canv.drawString(LEFT_MARGIN * 2, HEIGHT,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(HEIGHT),
                              'Período Facturado Desde:  ')
 
         self.canv.setFont(FONT, 10)
-        self.canv.drawString(LEFT_MARGIN * 2 + text_len, HEIGHT, self.since)
+        self.canv.drawString(LEFT_MARGIN * 2 + text_len,
+                             self.inv(HEIGHT), self.since)
 
         self.canv.setFont(BOLD_FONT, 10)
         text_len = canv.stringWidth(
             'Hasta:  ', BOLD_FONT, 10)
-        self.canv.drawString(LEFT_MARGIN * 2 + 210, HEIGHT,
+        self.canv.drawString(LEFT_MARGIN * 2 + 210, self.inv(HEIGHT),
                              'Hasta:  ')
         self.canv.setFont(FONT, 10)
         self.canv.drawString(LEFT_MARGIN * 2 + 210 +
-                             text_len, HEIGHT, self.to)
+                             text_len, self.inv(HEIGHT), self.to)
 
         self.canv.setFont(BOLD_FONT, 10)
         text_len = canv.stringWidth(
             'Fecha de Vto. para el pago:  ', BOLD_FONT, 10)
-        self.canv.drawString(LEFT_MARGIN * 2 + 330, HEIGHT,
+        self.canv.drawString(LEFT_MARGIN * 2 + 330, self.inv(HEIGHT),
                              'Fecha de Vto. para el pago:  ')
         self.canv.setFont(FONT, 10)
         self.canv.drawString(LEFT_MARGIN * 2 + 330 +
-                             text_len, HEIGHT, self.payment_vto)
+                             text_len, self.inv(HEIGHT), self.payment_vto)
 
     def generate_client_information(self):
         POINTER = 194
         I_HEIGHT = 62
         PADDING = 15
-        self.canv.rect(LEFT_MARGIN, 194, width -
+        self.canv.rect(LEFT_MARGIN, self.inv(POINTER + I_HEIGHT), width -
                        LEFT_MARGIN * 2, I_HEIGHT, stroke=1)
 
         self.canv.setFont(BOLD_FONT, 9)
 
-        self.canv.drawString(LEFT_MARGIN * 2, POINTER +
-                             PADDING, f'CUIT: {self.doc}')
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(POINTER +
+                             PADDING), f'CUIT: {self.doc}')
 
-        self.canv.drawString(LEFT_MARGIN * 2, POINTER + PADDING * 2 + 4,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(POINTER + PADDING * 2 + 4),
                              f'Condición frente al IVA:   {self.iva_status}')
 
-        self.canv.drawString(LEFT_MARGIN * 2, POINTER + PADDING * 3 + 9,
+        self.canv.drawString(LEFT_MARGIN * 2, self.inv(POINTER + PADDING * 3 + 9),
                              f'Condición de venta:   {self.sale}')
 
     def generate_total(self):
@@ -205,26 +232,26 @@ class PdfGenerator():
         PADDING = 20
         self.canv.setFont(BOLD_FONT, 10)
         self.canv.setLineWidth(STRONG_XL_STROKE)
-        self.canv.rect(LEFT_MARGIN, POINTER, width -
+        self.canv.rect(LEFT_MARGIN, self.inv(POINTER + I_HEIGHT), width -
                        LEFT_MARGIN * 2, I_HEIGHT, stroke=1)
 
         POINTER = 600
-        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, POINTER +
-                                  PADDING, 'Subtotal: $')
+        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, self.inv(POINTER +
+                                  PADDING), 'Subtotal: $')
 
-        self.canv.drawRightString(width - LEFT_MARGIN * 2, POINTER +
-                                  PADDING, self.subtotal)
+        self.canv.drawRightString(width - LEFT_MARGIN * 2, self.inv(POINTER +
+                                  PADDING), self.subtotal)
 
-        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, POINTER + PADDING * 2,
+        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, self.inv(POINTER + PADDING * 2),
                                   'Importe Otros Tributos: $')
 
-        self.canv.drawRightString(width - LEFT_MARGIN * 2, POINTER +
-                                  PADDING * 2 + 4, self.taxes)
+        self.canv.drawRightString(width - LEFT_MARGIN * 2, self.inv(POINTER +
+                                  PADDING * 2 + 4), self.taxes)
         self.canv.setFont(BOLD_FONT, 11)
-        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, POINTER + PADDING * 3,
+        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, self.inv(POINTER + PADDING * 3),
                                   'Importe Total: $')
 
-        self.canv.drawRightString(width - LEFT_MARGIN * 2, POINTER + PADDING * 3,
+        self.canv.drawRightString(width - LEFT_MARGIN * 2, self.inv(POINTER + PADDING * 3),
                                   self.total)
 
     def generate_footer(self):
@@ -232,24 +259,25 @@ class PdfGenerator():
         PADDING = 10
 
         img = qrcode.make(self.qrdata)
-        self.canv.drawCentredString(width/2, POINTER + PADDING, f'Pág. 1/1')
-        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, POINTER + PADDING,
+        self.canv.drawCentredString(
+            width/2, self.inv(POINTER + PADDING), f'Pág. 1/1')
+        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, self.inv(POINTER + PADDING),
                                   'CAE N:')
 
-        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, POINTER + PADDING * 3,
+        self.canv.drawRightString(LEFT_MARGIN * 2 + width * 3/4, self.inv(POINTER + PADDING * 3),
                                   'Fecha de Vto. de CAE:')
 
         self.canv.setFont(FONT, 10)
-        self.canv.drawString(LEFT_MARGIN * 2 + width * 3/4 + 10, POINTER + PADDING,
+        self.canv.drawString(LEFT_MARGIN * 2 + width * 3/4 + 10, self.inv(POINTER + PADDING),
                              self.cae)
-        self.canv.drawString(LEFT_MARGIN * 2 + width * 3/4 + 10, POINTER + PADDING * 3,
+        self.canv.drawString(LEFT_MARGIN * 2 + width * 3/4 + 10, self.inv(POINTER + PADDING * 3),
                              self.vto_cae)
 
         self.canv.drawInlineImage(img, LEFT_MARGIN * 2,
-                                  POINTER - 90, width=90, height=90)
+                                  self.inv(POINTER + 90), width=90, height=90)
 
         self.canv.drawImage('assets/afip_img.png', LEFT_MARGIN * 2 + 100,
-                            POINTER + 20, height=45, width=100)
+                            self.inv(POINTER + 20 + 45), height=45, width=100)
 
 
 a = PdfGenerator('0001', '0000004', '05/05/2022', '20396423295', '20396423295', '01/01/1900', 'Tomas Nocetti',
